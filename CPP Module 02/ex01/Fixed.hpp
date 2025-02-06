f2364a2a277ebf7fcc1701f640a6d9b311a41090
Arthur Bernard <artberna@paul-f4Br3s7.clusters.42paris.fr>
2025-02-06 15:44:43 +0100

    ex01,02

--- CPP Module 02/ex01/Fixed.hpp
+++ CPP Module 02/ex01/Fixed.hpp
@@ -22,7 +22,7 @@ class Fixed{
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 		float	toFloat(void) const;
-		int		toInt void) const;
+		int		toInt (void) const;
 };
 
 std::ostream&	operator<<(std::ostream& o, Fixed const& rhs);
